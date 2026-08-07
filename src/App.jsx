c312b2273882b18b72bf069d4bf873333ad246e1
GEPratama <axelblade21@gmail.com>
2026-08-07 12:35:34 +0700

    display update

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,10 @@ import "./styles/global.css";
 function App() {
   return (
     <>
+      <div className="shape shape1"></div>
+      <div className="shape shape2"></div>
+      <div className="shape shape3"></div>
+
       <Navbar />
 
       <main className="container">
